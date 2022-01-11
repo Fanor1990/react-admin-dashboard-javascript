fb7d406ad820565d6f8f9ae95d2dc85d06375915
Fanor Choque Aguirre <fanorarmandochoque@gmail.com>
2022-01-10 22:51:25 -0500

    Tipografia

--- src/App.js
+++ src/App.js
@@ -1,67 +1,28 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
+import Typography from '@material-ui/core/Typography'
 
-import {Icon, IconButton} from "@material-ui/core"
 
 //props de botones color, variant
 //lavesitas truquito para importar todo el conenido de material ui de la carpetita
 function App() {
   return (
     <div>
-      <h1>Botones Color Fuente</h1>
-      <Button color="default">textcolordefault</Button>
+      <Typography variant="h1" color="initial">
+      DevFanor
+      </Typography>
 
-      <Button color="primary">textcolorprimary</Button>
+      <Typography variant="body1" color="secondary" align="center" paragraph gutterBottom>
+      Login
+      </Typography>
 
-      <Button color="secondary">txtcolorsecondary</Button>
+      <Typography variant="body1" color="secondary" align="center" paragraph gutterBottom>
+      Nombre de Usuario
+      </Typography>
 
-      <Button color="inherit">txtcolorinherit</Button>
-
-      <Button variant="text" color="default">
-        fuentevarianttext
-      </Button>
-
-      <Button variant="contained" color="primary">
-        fuentevariantcontained
-      </Button>
-
-      <Button variant="outlined" color="secondary">
-        fuentevariantoutlined
-      </Button>
-
-      <Button variant="contained" color="inherit">
-        fuentevariantcontained
-      </Button>
-
-      <Button variant="contained" color="inherit" disableElevation>
-        fuentedisableelevation
-      </Button>
-
-      <Button variant="contained" color="secondary" href="https://google.com">
-       Google
-      </Button>
-
-      <Button variant="contained" color="secondary" fullWidth>
-       fullWhidth
-      </Button>
-
-      <Button variant="contained" color="secondary" size="small">
-       small
-      </Button>
-      <Button variant="contained" color="secondary" size="large">
-       large
-      </Button>
-
-      <Button variant="contained" color="secondary" size="medium">
-       medium
-      </Button>
-
-      
-      <Icon>room</Icon>
-
-      <IconButton aria-label="delete" >
-        Delete
-      </IconButton>
+      <Typography variant="body1" color="secondary" align="center" gutterBottom>
+      Contraseña
+      </Typography>
+     
       
     </div>
   );
