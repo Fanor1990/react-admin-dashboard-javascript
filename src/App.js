30684b0989e2f839f008df892a889d78c75d5e5e
Fanor Choque Aguirre <fanorarmandochoque@gmail.com>
2022-01-11 00:13:09 -0500

    temaglobalthem

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
 import React from "react";
 import Button from "@material-ui/core/Button"
-import Typography from '@material-ui/core/Typography';
-import {makeStyles} from "@material-ui/core/styles"
 
-const useStyles = makeStyles({
-  bottonfanor: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    border: 0,
-    borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    height: 48,
-    padding: '0 30px',
-  },
-});
+import { ThemeProvider } from "@material-ui/core/styles"
+import theme from "./temaconfig"
+
+
 function App() {
 
-  const clasess = useStyles()
 
   return (
-    <div>
-      <Button className={clasess.bottonfanor}>
-      botonfanorpersonalizado
+    
+      <ThemeProvider theme={theme}>
+      <Button variant="contained" color="primary">
+        boton
       </Button>
+      <Button variant="contained" color="secondary">
+        boton
+      </Button>
+      </ThemeProvider>
+     
       
-      
-    </div>
+    
   );
 }
 
